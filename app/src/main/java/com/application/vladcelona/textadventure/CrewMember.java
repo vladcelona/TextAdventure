783953308594c0c21813e7df43fa4042ec83db36
vladcelona <vladbalandin2013@gmail.com>
2023-03-07 00:06:41 +0300

    Final version of the code, might be updated later

--- app/src/main/java/com/application/vladcelona/textadventure/CrewMember.java
+++ app/src/main/java/com/application/vladcelona/textadventure/CrewMember.java
@@ -6,12 +6,14 @@ public class CrewMember {
     private int energyLevel;
 
     CrewMember() {
-        this.oxygenLevel = 100; this.energyLevel = 100;
+        oxygenLevel = 100; energyLevel = 100;
     }
 
-    public void reduceOxygen() { this.oxygenLevel -= 5; }
-    public void reduceEnergy() { this.energyLevel -= 5; }
-    public void reduceEnergyWhileShooting() { this.energyLevel -= 10; }
+    public void reduceOxygen() { oxygenLevel -= 5; }
+    public void reduceEnergy() { energyLevel -= 5; }
+    public void reduceEnergyShooting() { energyLevel -= 10; }
 
     public boolean checkStats() { return oxygenLevel > 0 && energyLevel > 0; }
+    public int getEnergyLevel() { return energyLevel; }
+    public int getOxygenLevel() {return oxygenLevel; }
 }
